fe6cb5ae093e88d4bcb799fb93f57ee6ec30676a
Matthias Scholz <morpho@gmx.de>
2013-05-03 09:03:10 +0200

    Changed settings structure

--- PROJECTNAME/urls.py
+++ PROJECTNAME/urls.py
@@ -2,20 +2,18 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    
-    # project specific urls  (index, impressum, etc)
-    url(r'^$', 'PROJECTNAME.apps.views.index', name='index'),
-    
-    # app specific urls
-    url(r'^APP/', include('PROJECTNAME.apps.APPNAME.urls')),
+    url(r'', include('PROJECTNAME.apps.core.urls'), namespace='core'),
+    url(r'', include('PROJECTNAME.apps.APP_NAME.urls'), namespace='APP_NAME'),
     
     url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
